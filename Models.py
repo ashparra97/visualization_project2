d435d819b2b790e481577eb8306a15c18626c5ff
KWildcatQ <77754927+KWildcatQ@users.noreply.github.com>
2021-07-21 20:09:32 -0700

    Add files via upload

--- Models.py
+++ Models.py
@@ -4,7 +4,10 @@ def create_classes(db):
 
         id = db.Column(db.Integer, primary_key=True)
         expenditures = db.Column(db.String(255))
-        
+        actual_fy_18_19 = db.Column(db.Money)
+        adopted_fy_19_20 = db.Column(db.Money)
+        estimated_fy_19_20 = db.Column(db.Money)
+        adopted_fy_20_21 = db.Column(db.Money)
 
         def __repr__(self):
             return '<Budget %r>' % (self.name)
