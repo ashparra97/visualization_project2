c64e29c199680e0898ab090daefa49117b2dbd8a
jasonwang <huayu.j.wang@gmail.com>
2021-07-21 21:35:52 -0700

    Stripped to starter codes.

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 Budget = create_classes(db)
-Arrests = create_classes(db)
+# Arrests = create_classes(db)
 
 # create route that renders index.html template
 @app.route("/")
@@ -41,22 +41,23 @@ def home():
 # Example 1: Two column table
 @app.route("/api/budget.json")
 def budget():
-    results = db.session.query(Budget.id, Budget.expenditures, Budget.actual_fy_18_19, Budget.adopted_fy_19_20, Budget.estimated_fy_19_20, Budget.adopted_fy_20_21).all()
+    results = db.session.query(Budget.id, Budget.expenditures).all()
 
+#  Budget.actual_fy_18_19, Budget.adopted_fy_19_20, Budget.estimated_fy_19_20, Budget.adopted_fy_20_21
     budget_id = [result[0] for result in results]
     budget_expenditures = [result[1] for result in results]
-    budget_actual_fy_18_19 = [result[2] for result in results]
-    budget_adopted_fy_19_20 = [result[3] for result in results]
-    budget_estimated_fy_19_20 = [result[4] for result in results]
-    budget_adopted_fy_20_21 = [result[5] for result in results]
+    # budget_actual_fy_18_19 = [result[2] for result in results]
+    # budget_adopted_fy_19_20 = [result[3] for result in results]
+    # budget_estimated_fy_19_20 = [result[4] for result in results]
+    # budget_adopted_fy_20_21 = [result[5] for result in results]
 
     Budget_data = [{
         "id": budget_id,
         "expenditures": budget_expenditures,
-        "actual_fy_18_19": budget_actual_fy_18_19,
-        "adopted_fy_19_20": budget_adopted_fy_19_20,
-        "estimated_fy_19_20": budget_estimated_fy_19_20,
-        "adopted_fy_20_21": budget_adopted_fy_20_21
+        # "actual_fy_18_19": budget_actual_fy_18_19,
+        # "adopted_fy_19_20": budget_adopted_fy_19_20,
+        # "estimated_fy_19_20": budget_estimated_fy_19_20,
+        # "adopted_fy_20_21": budget_adopted_fy_20_21
 
     }]
 
