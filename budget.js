fed3402e5892fd247d5bdd29733e794f88b51324
knorman123 <55822444+knorman123@users.noreply.github.com>
2021-07-21 20:12:03 -0700

    add test code for budget.js

---
+++ budget.js
@@ -0,0 +1,29 @@
+// Create the data array for the plot
+// Test code
+d3.json("api/budget.json").then((Budget_data) => {
+
+    console.log(Budget_data);
+
+    var x = Budget_data[0].id
+
+    var y = Budget_data[0].expenditures
+
+    var trace = {
+        x: x,
+        y: y,
+        type: "bar"
+    };
+
+    var data = [trace];
+
+    // Define the plot layout
+    var layout = {
+    title: "Budget Data",
+    xaxis: { title: "ID" },
+    yaxis: { title: "Expenditures"}
+    };
+
+    // Plot the chart to a div tag with id "bar-plot"
+    Plotly.newPlot("bar-plot", data, layout);
+
+});
