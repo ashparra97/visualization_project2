6e52c909d9017c256e2d79af98ad50b8733f07ed
jasonwang <huayu.j.wang@gmail.com>
2021-07-24 15:07:57 -0700

    Year 2019 test

--- static/js/arrests_gender_years.js
+++ static/js/arrests_gender_years.js
@@ -5,10 +5,18 @@ d3.json("../data/arrests.json").then((Arrests_data) => {
 
     // console.log(Arrests_data);
     // console.log(Arrests_data[0]["booking id"].length)
-    arrest_id = Arrests_data[0]["booking id"];
-    for (i = 0; i < arrest_id.length; i++){
-        console.log(arrest_id[i])
+    arrest_id = [];
+    for (i = 0; i < Arrests_data[0]["booking id"].length; i++){
+        if (Arrests_data[0].year[i] == 2019){
+            // console.log(Arrests_data[0]["booking id"][i]);
+            // console.log(Arrests_data[0]["booking id"][i] == Arrests_data[0]["booking id"][i - 1]);
+            if (Arrests_data[0]["booking id"][i] !== Arrests_data[0]["booking id"][i - 1]){
+                arrest_id.push(Arrests_data[0]["booking id"][i]);
+            };
+        };
+    
     }
     console.log(arrest_id);
-    // console.log(arrest_id.length);
+    console.log(arrest_id.length);
 });
+
