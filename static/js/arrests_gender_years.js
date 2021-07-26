3b1c1ef4bf36b2c9017f02618de86042154b986f
Ksquinn86 <Kellen.Quinn@gmail.com>
2021-07-26 16:45:48 -0700

    Switching back

--- static/js/arrests_gender_years.js
+++ static/js/arrests_gender_years.js
@@ -121,8 +121,6 @@ d3.json("api/arrests.json").then((response) => {
     var layout = {
         title: "Tucson PD 2019 - 2021 Arrest Data",
         xaxis: { title: "Year" },
-            tickmode = 'array',
-            tickvals = [2019, 2020, 2021],
         yaxis: { title: "Number of Arrests"},
         barmode: 'stack'
     };
