6659bebe484abcafc5fb269895f53d7c3e52ca09
jasonwang <huayu.j.wang@gmail.com>
2021-07-25 18:02:27 -0700

    Updated app.js for testing.

--- static/js/app.js
+++ static/js/app.js
@@ -62,16 +62,16 @@ function plot2019(){
             arrestMonths_19.splice(i, 1, months[`${arrestMonths_19[i]}`]);
         };
     
-        // // TESTING
-        // var arrests_by_months = {}; 
-        // for (var i = 0; i < arrestMonths_19.length; i++){
-        //     arrests_by_months[arrestMonths_19[i]] = arrestCounts_19[i];
-        // };
-        // console.log(arrests_by_months);
-        // // TESTING ENDS
+        // TESTING
+        var arrests_by_months = {}; 
+        for (var i = 0; i < arrestMonths_19.length; i++){
+            arrests_by_months[arrestMonths_19[i]] = arrestCounts_19[i];
+        };
+        console.log(arrests_by_months);
+        // TESTING ENDS
 
-        // console.log(arrestMonths_19);
-        // console.log(arrestCounts_19);
+        console.log(arrestMonths_19);
+        console.log(arrestCounts_19);
 
         // Plot graph
         var trace1 = {
